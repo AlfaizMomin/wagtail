39520198de50e9c5bf1a8aaa48eb8a3653ef5564
AlfaizMomin <alfaiz.momin1998@gmail.com>
2023-10-03 22:45:45 +0530

    Update rebuild-translation-sources.sh

--- scripts/rebuild-translation-sources.sh
+++ scripts/rebuild-translation-sources.sh
@@ -3,6 +3,7 @@
 # Delete old translation sources
 find ../wagtail -iname *.po -iwholename */en/* -delete
 
+
 # Run makemessages on each app
 for d in $(find ../wagtail -iwholename */locale/* | sed 's|\(.*\)/locale.*|\1|' | sort -u);
 do
